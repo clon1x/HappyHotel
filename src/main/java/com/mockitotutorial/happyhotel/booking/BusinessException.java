1f45de1d39e30decf2e3ef2cc50c6b14ae3d045a
LVL <luis.vinelerma@yahoo.com>
2023-04-12 20:07:10 +0200

    Initial Project State
    4. Eclipse Project Setup

---
+++ src/main/java/com/mockitotutorial/happyhotel/booking/BusinessException.java
@@ -0,0 +1,10 @@
+package com.mockitotutorial.happyhotel.booking;
+
+public class BusinessException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3352679405221044346L;
+
+}
